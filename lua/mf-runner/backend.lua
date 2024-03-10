5e4cd165899291ba7d310fb1c5ac1c7b8fb31368
sb <simon@burgr.io>
2024-03-10 22:16:27 +0100

    should work now

--- lua/mf-runner/backend.lua
+++ lua/mf-runner/backend.lua
@@ -1,18 +1,18 @@
 local M = {}
 
 function M.run_makefile(option)
-	vim.notify("running" .. "!make" .. option)
-	vim.cmd("!make " .. option)
+	local command = "make " .. option
+	vim.notify("running " .. command, vim.log.levels.info)
+	require("toggleterm").exec_command('cmd="' .. command .. '"')
 end
 
 function M.create_makefile()
-	local utils = require("mf-runner.utils")
+	local utils = require "mf-runner.utils"
 	local filepath = utils.os_path(vim.fn.getcwd() .. "\\Makefile")
-	-- FIX: THIS DOESNT WORK FOR SOME REASON
-	if vim.fn.glob(filepath) then
+	if vim.fn.filereadable(filepath) == 1 then
 		vim.notify("File already exists", vim.log.levels.INFO)
 	else
-		vim.cmd.edit("Makefile")
+		vim.cmd.edit "Makefile"
 	end
 end
 
